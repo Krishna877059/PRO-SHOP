ac0b9f8e4346cf6dbad47a0d3a1b11d3999066e5
krishna <ks1150223@gmail.com>
2022-07-27 16:19:08 +0530

    fetching product details through redux

--- my-app/src/screens/HomeScreen.js
+++ my-app/src/screens/HomeScreen.js
@@ -3,6 +3,9 @@ import React,{useState,useEffect} from 'react'
 import { Row,Col } from 'react-bootstrap'
 import Product from '../components/Product'
  import { listProducts } from '../actions/productActions'
+
+ import Message from '../components/Message'
+ import Loader from '../components/Loader'
 const HomeScreen = () => {
  const dispatch = useDispatch()
  const productList = useSelector(state => state.productList)
@@ -14,8 +17,8 @@ const HomeScreen = () => {
   return (
     <>
        <h1>Latest Products</h1>
-       {loading ?( <h1>Loading...</h1>)
-       : error?  <h3>{error}</h3>:
+       {loading ?( <Loader/>)
+       : error?  <Message variant='danger'>{error}</Message>:
        <Row>
             {products.map(product =>(
                 <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
--- my-app/src/screens/ProductScreen.js
+++ my-app/src/screens/ProductScreen.js
@@ -2,21 +2,17 @@ import React,{useEffect,useState} from 'react'
 import { Link,useParams } from 'react-router-dom'
 import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
 import Rating from '../components/Rating'
-
-import axios from 'axios'
-
-const ProductScreen = ( match ) => {
-    const { id } =useParams("");
-    const [product, setProduct] = useState({})
-
+import { useDispatch,useSelector } from 'react-redux'
+import { listProductDetails } from '../actions/productActions'
+const ProductScreen = ( {match}) => {
+    const { id } =useParams();
+    const dispatch = useDispatch()
+const productDetails = useSelector((state) =>state.productDetails)
+const {loading,error,product} = productDetails
   useEffect(()=>{
-    const fetchProduct = async () => {
-      const {data} = await axios.get(`/api/products/${id}`)
-      setProduct(data)
-    }
-    fetchProduct()
-  },)
-    
+    dispatch(listProductDetails(id)) 
+  },[dispatch,match])
+   
   return (
     <>
      <Link className='btn btn-dark my-3' to='/'>Go Back</Link>
