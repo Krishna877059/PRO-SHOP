2eb34a6d9ccd555764f82a13b884a7c0b4c21e54
krishna <ks1150223@gmail.com>
2022-07-31 01:13:18 +0530

    Backend authentication ended

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
 
 
 //desc fetch all products
@@ -7,6 +8,68 @@ import User from '../models/userModel.js'
 // access fetch all products/public routes
 const authUser = asyncHandler(async(req,res) =>{
     const { email, password } = req.body
-    res.send(email,password)
+    const user = await User.findOne({ email })
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user._id),
+        })
+      } else {
+        res.status(401)
+        throw new Error('Invalid email or password')
+      }
 })
-export { authUser}
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+})
+//desc@ Get user profile
+// route: get /api/users/profile
+// access Private
+const getUserProfile = asyncHandler(async(req,res)=>{
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+export { authUser,getUserProfile,registerUser} 
